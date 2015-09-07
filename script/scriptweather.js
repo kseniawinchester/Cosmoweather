c5951b0662c913eb06e5bba88392cbb40bfed2a6
Ksenia Terentyeva <kseniawinchester@gmai.com>
2015-09-07 12:54:40 +0000

    i think the last commit

--- script/scriptweather.js
+++ script/scriptweather.js
@@ -1,55 +1,60 @@
-$(document).ready(function () {
 
-	navigator.geolocation.getCurrentPosition(showPosition);
+	function firstInitialization() {
+		navigator.geolocation.getCurrentPosition(function (position) {
+			var lat = position.coords.latitude;
+			var lon = position.coords.longitude;
+			initializeDay(0, lat, lon);
+			initializeDay(1, lat, lon);
+			initializeDay(2, lat, lon);
+			initializeDay(3, lat, lon);
+			initializeDay(4, lat, lon);
 
-	function showPosition(position) {
 
-		var lat = position.coords.latitude;
-		var lon = position.coords.longitude;
+		});
+	}
+
+	function initializeDay(dayNumber, lat, lon) {
+
+		var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+		var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
 
-		$.get("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "s", function (data) {
+		$.get("http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "s", function (data1) {
+
+			var temp = data1.list[dayNumber * 8].main.temp;
+			var tempCel = Math.round(temp - 273) + 'C';
+			var icon = data1.list[dayNumber * 8].weather[0].icon;
+			var description = data1.list[dayNumber * 8].weather[0].description;
+			var humidity = data1.list[dayNumber * 8].main.humidity + '%';
+			var city = data1.city.name;
+
 
 			var today = new Date();
-			var day1 = new Date(today.getTime() + 86400000);
-			var day2 = new Date(day1.getTime() + 86400000);
-			var day3 = new Date(day2.getTime() + 86400000);
+			var day1 = new Date(today.getTime() + dayNumber * 86400000);
+
+			var day1Formatted = days[day1.getUTCDay()] + ' ' + day1.getDate() + '.' + months[day1.getMonth()] + '.' + day1.getFullYear();
 
 
-			
-			var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-			 var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-			var todayFormated = days[today.getUTCDay()] +' '+ today.getDate() + '.' + months[today.getMonth()]+ '.' + today.getFullYear();
-			var day1Formatted = days[day1.getUTCDay()]+' '+day1.getDate() + '.' + months[day1.getMonth()] + '.' + day1.getFullYear();
-			var day2Formatted = days[day2.getUTCDay()]+' '+day2.getDate() + '.' + months[day2.getMonth()] + '.' + day2.getFullYear();
-			var day3Formatted = days[day3.getUTCDay()]+' '+day3.getDate() + '.' + months[day3.getMonth()] + '.' + day3.getFullYear();
-
-
-
-
-			var temp = data.main.temp;
-			var city = data.name;
-			var tempCel = Math.round(converKel(temp)) + '\u2103';
-			var icon = data.weather[0].icon;
-			var description = data.weather[0].description;
-			var humidity = data.main.humidity + '%';
-
-			$('#description').text(description);
-			$('#icon').html('<img src="images/' + icon + '.png"/>')
-			$('#temp').text(tempCel);
-			$('#humidity').text(humidity);
+			$('#description' + dayNumber).text(description);
+			$('#icon' + dayNumber).html('<img src="images/' + icon + '.png"/>')
+			$('#temp' + dayNumber).text(tempCel);
+			$('#humidity' + dayNumber).text(humidity);
+			$('#day' + dayNumber).text(day1Formatted);
 			$('#location').text(city);
-			$('#date').text(todayFormated);
 		});
 
+	};
+
+	function initializeDayForCity(latCity, lonCity) {
+		initializeDay(0, latCity, lonCity);
+		initializeDay(1, latCity, lonCity);
+		initializeDay(2, latCity, lonCity);
+		initializeDay(3, latCity, lonCity);	
+		initializeDay(4, latCity, lonCity);
+
 	}
+$(document).ready(function () {
 
-
-
-
-	function converKel(kelv) {
-		var cel = kelv - 273;
-		return cel;
-	}
+	firstInitialization();
 
 });
